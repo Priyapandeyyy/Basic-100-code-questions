32800f4acced9d81bc259c39d9ac0052deef460f
Priya Pandey <priya.pandeyy321@gmail.com>
2024-05-28 18:35:38 +0530

    Second Commit

---
+++ src/SumofNumbersinInteger.java
@@ -0,0 +1,11 @@
+// We will solve this using three methods
+
+// Method 1 : Using Brute Force
+// Method 2 : Using the Formula
+// Method 3 : Using Recursion
+public class SumofNumbersinInteger {
+	public static void main (String[] args) {
+		
+		// Method 1 : Using Brute Force
+	}
+}
